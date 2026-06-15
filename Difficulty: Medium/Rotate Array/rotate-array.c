1de2637db0e1d007b363a3ea21ef5865abc504d6
shubh dubey <shubhdubey8756@gmail.com>
2026-06-15 13:01:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Rotate Array/rotate-array.c
@@ -0,0 +1,19 @@
+void reverse(int arr[], int start, int end) {
+    while(start < end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+
+        start++;
+        end--;
+    }
+}
+
+void rotateArr(int arr[], int n, int d) {
+
+    d = d % n;
+
+    reverse(arr, 0, d - 1);
+    reverse(arr, d, n - 1);
+    reverse(arr, 0, n - 1);
+}
