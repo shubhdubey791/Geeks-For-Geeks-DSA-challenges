e3ad58f27d23967c930c8e8127e85487de2be0c8
shubh dubey <shubhdubey8756@gmail.com>
2026-08-16 00:19:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Arithmetic Operators/arithmetic-operators.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        int p = x +y;
+        int q = x - y;
+        int r = x *y;
+        
+        int t = x/y;
+        int u = x%y;
+        double b = x;
+        
+        double s = b/y;
+        
+        
+
+        System.out.printf("%d %d %d %.3f %d %d", p, q, r, s, t, u);
+    }
+}
