711d1294a3c9cee90bc83a2ecd006ce24cbc21b5
shubh dubey <shubhdubey8756@gmail.com>
2026-08-17 13:22:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Greatest of three numbers/greatest-of-three-numbers.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        if(a>b && a>c)
+        System.out.print(a);
+      else if(a<b && b>c)
+        System.out.print(b);
+        else
+        System.out.print(c +" is greatest among three");
+    }
+}
