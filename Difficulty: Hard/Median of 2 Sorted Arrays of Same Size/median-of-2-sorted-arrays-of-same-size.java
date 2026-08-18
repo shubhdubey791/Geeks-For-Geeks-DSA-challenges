3aa3517d7ee1d898ddc82b3721021603f73350b3
shubh dubey <shubhdubey8756@gmail.com>
2026-08-18 11:22:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Median of 2 Sorted Arrays of Same Size/median-of-2-sorted-arrays-of-same-size.java
@@ -0,0 +1,40 @@
+class Solution {
+    public double medianOf2(int a[], int b[]) {
+        int[] ans  = new int[a.length+b.length];
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        double median;
+
+        while(i<a.length && j<b.length){
+            if(a[i]<=b[j]){
+                ans[k] = a[i];
+                k++;
+                i++;
+            }
+            else{
+                ans[k] = b[j];
+                k++;
+                j++;
+            }
+        }
+
+         while(i<a.length ){
+              ans[k] = a[i];
+                k++;
+                i++;
+         }
+          while(j<b.length ){
+              ans[k] = b[j];
+                k++;
+                j++;
+         }
+         int n = ans.length;
+         if(n%2!=0)
+        median =ans[(n+1)/2-1];
+        else
+        median =((double)(ans[n/2-1] + ans[n/2]))/2;
+        return median;
+        
+    }
+}
