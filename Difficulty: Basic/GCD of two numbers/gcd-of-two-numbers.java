e14297f3f7d6a67a4a3c7c24af350db1d8d57547
shubh dubey <shubhdubey8756@gmail.com>
2026-09-02 07:52:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/GCD of two numbers/gcd-of-two-numbers.java
@@ -0,0 +1,16 @@
+class Solution {
+    public static int gcd(int a, int b) {
+    while(b>0){
+        int rem  = a%b;
+        a=b;
+        b= rem;
+        
+        
+        
+        
+        
+    }
+    return a;
+        
+    }
+}
